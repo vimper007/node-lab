4447c804b0f98443f9d9b007d797cea3cb0349e3
Vimukthi Perera <vimper007@gmail.com>
2026-09-09 20:53:34 +0530

    chore: comment out database and authentication environment variables in schema

--- env.ts
+++ env.ts
@@ -21,10 +21,10 @@ const envSchema = z.object({
   APP_STAGE: z.enum(['dev', 'test', 'production']).default('dev'),
 
   PORT: z.coerce.number().positive().default(3000),
-  DATABASE_URL: z.string().startsWith('postgresql://'),
-  JWT_SECRET: z.string().min(32, 'Must be 32 chars long'),
-  JWT_EXPIRES_IN: z.string().default('7d'),
-  BCRYPT_ROUNDS: z.coerce.number().min(10).max(20).default(12),
+  // DATABASE_URL: z.string().startsWith('postgresql://'),
+  // JWT_SECRET: z.string().min(32, 'Must be 32 chars long'),
+  // JWT_EXPIRES_IN: z.string().default('7d'),
+  // BCRYPT_ROUNDS: z.coerce.number().min(10).max(20).default(12),
 })
 
 export type Env = z.infer<typeof envSchema>
