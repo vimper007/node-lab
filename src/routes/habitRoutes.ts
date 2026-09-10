ddebd42546dada4e885c85fe617fae6c5c4409c6
Vimukthi Perera <vimper007@gmail.com>
2026-09-10 11:17:24 +0530

    feat: implement habit routes with request validation schemas

--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -1,9 +1,44 @@
 import { Router } from 'express'
+import { validateBody, validateParams } from '../middleware/validation.ts'
+import { z } from 'zod'
+
+const createHabitSchema = z.object({
+  name: z.string(),
+})
+
+const completeParamsSchema = z.object({
+  id: z.string().max(3),
+})
 
 const router = Router()
 
+
+
+
+
 router.get('/', (req, res) => {
-  res.json({ message: 'habit' })
+  res.json({ message: 'habits' })
 })
+
+router.get('/:id', (req, res) => {
+  res.json({ message: 'got one habbit' })
+})
+
+router.post('/', validateBody(createHabitSchema), (req, res) => {
+  res.json({ message: 'created habit' }).status(201)
+})
+
+router.delete('/:id', (req, res) => {
+  res.json({ message: 'deleted habit' })
+})
+
+router.post(
+  '/:id/complete',
+  validateParams(completeParamsSchema),
+  validateBody(createHabitSchema),
+  (req, res) => {
+    res.json({ message: 'completed habit' }).status(201)
+  }
+)
 
 export default router
