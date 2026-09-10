c695a5c4867d30cd35de07b6c962890001291e64
Vimukthi Perera <vimper007@gmail.com>
2026-09-10 11:16:25 +0530

    feat: implement user routes for CRUD operations

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,7 +3,19 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.json({ message: 'user' })
+  res.json({ message: 'users' })
+})
+
+router.get('/:id', (req, res) => {
+  res.json({ message: 'got user' })
+})
+
+router.put('/:id', (req, res) => {
+  res.json({ message: 'user updated' })
+})
+
+router.delete('/:id', (req, res) => {
+  res.json({ message: 'user deleted' })
 })
 
 export default router
